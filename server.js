206600cf0535f6c39f1b7d01cfd8c2b512c4a57d
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-06 09:12:51 +0100

    I like how app progess, now i will do the dom manipulation with JS

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+  socket.emit('message', 'Welcome to FB-Chat');
+
+  socket.broadcast.emit('message', 'A user has joind the chat');
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  });
+
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', msg);
+  })
+})
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log('Server running');
-})
+});
